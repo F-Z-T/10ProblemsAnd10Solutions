f3304df0b2c24d129008a2da9b38e5d178efe968
Fatma Zehra Turhan <61631047+F-Z-T@users.noreply.github.com>
2021-02-21 19:30:49 +0300

    Add files via upload

---
+++ S2/S2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Bir Sayı Giriniz :");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            
+            for (int i = 1; i <= sayi / 2; i++)
+            {
+
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            Console.WriteLine("");
+            
+            if (sayi == toplam)
+            {
+                Console.WriteLine(sayi + " Sayısı  Mükemmel Sayıdır");
+            }
+            else
+            {
+                Console.WriteLine(sayi + " Sayısı  Mükemmel Sayı Değildir");
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
