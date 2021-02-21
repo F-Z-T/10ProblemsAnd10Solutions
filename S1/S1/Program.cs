757f7a29cb27d31258852b866c31e4fd65dd88c2
Fatma Zehra Turhan <61631047+F-Z-T@users.noreply.github.com>
2021-02-21 19:25:47 +0300

    Add files via upload
    Klavyeden girilen bir tamsayının hanelerindeki en büyük tamsayıyı ekrana yazdıran
    program.

---
+++ S1/S1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace S1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            
+            int a, enB, b, sayi;
+            Console.WriteLine("sayı giriniz");
+    
+            a = Convert.ToInt32(Console.ReadLine());
+            enB = 0;
+
+            while (a != 0)
+            {
+                b = a - a / 10 * 10;
+                if (enB < b)
+                {
+                    enB = b;
+                }
+                a = a / 10;
+
+            }
+
+            Console.WriteLine("En büyük değer : " + enB);
+
+        }
+    }
+}
+
+
+
+
