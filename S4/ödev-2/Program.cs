5592d30010dbccab2dec084d27bdafe7686d594d
Fatma Zehra Turhan <61631047+F-Z-T@users.noreply.github.com>
2021-02-21 19:54:15 +0300

    Add files via upload

---
+++ S4/ödev-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ödev_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kenar1, kenar2, kenar3;
+
+            Console.WriteLine("kenar1 uzunluğunu giriniz");
+            kenar1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("kenar2 uzunluğunu giriniz");
+            kenar2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("kenar3 uzunluğunu giriniz");
+            kenar3 = Convert.ToInt32(Console.ReadLine());
+
+            if (((kenar1 + kenar2) > kenar3) && (Math.Abs(kenar1 - kenar2) < kenar3) && ((kenar1 + kenar3) > kenar2) && (Math.Abs(kenar1 - kenar3) < kenar2) && ((kenar3 + kenar2) > kenar1) && (Math.Abs(kenar3 - kenar1) < kenar1))
+            {
+                Console.WriteLine("{0}, {1} ve {2} kenarları ile üçgen oluşturulabilir.", kenar1, kenar2, kenar3);
+
+            }
+            else 
+            {
+                Console.WriteLine("{0}, {1} ve {2} kenarları ile üçgen oluşturulamaz.", kenar1, kenar2, kenar3);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
